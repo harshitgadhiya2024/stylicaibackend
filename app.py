ead930222df93b59b0fb9861d2acc811f3c2896e
harshittrsthealth <150043423+harshittrsthealth@users.noreply.github.com>
2025-03-06 17:56:16 +0530

    new changes

--- app.py
+++ app.py
@@ -78,14 +78,17 @@ def view_logs():
         return {"message": "data is not present"}
 
 def pil_to_binary_mask(pil_image, threshold=0):
+    print("new1")
     np_image = np.array(pil_image)
     grayscale_image = Image.fromarray(np_image).convert("L")
     binary_mask = np.array(grayscale_image) > threshold
     mask = np.zeros(binary_mask.shape, dtype=np.uint8)
+    print("new2")
     for i in range(binary_mask.shape[0]):
         for j in range(binary_mask.shape[1]):
             if binary_mask[i,j] == True :
                 mask[i,j] = 1
+    print("new3")
     mask = (mask*255).astype(np.uint8)
     output_mask = Image.fromarray(mask)
     return output_mask
